7a01c325a282fd03cc100455cc455a3c24b19b7a
Ryan Sullivan <ryan@kayoticlabs.com>
2011-11-18 18:39:59 -0500

    Added Setup directory and readme file.

---
+++ README.txt
@@ -0,0 +1,96 @@
+==============================================================
+	Mineshafter Squared is a replacement for the 
+	Minecraft Authentication, Skin, and Cape systems.
+    Copyright (C) 2011  Ryan Sullivan
+	
+    Contributors: Ryan Sullivan
+    Company: Kayotic Labs (www.kayoticlabs.com)
+    Webapp: Mineshafter Squared (www.mineshaftersquared.com)
+==============================================================
+
+This is all assumed you have a webhost that offers 
+the following features, or have installed them yourself
+on a computer you wish to use as the authentication server.
+
+1. Webserver (Apache has been tested, but it should work in others)
+2. PHP
+3. MySQL
+4. PHPMyAdmin
+
+Table of Contents
+	I. Installation
+		1. Database Setup
+		2. Config Setup
+		3. Upload Files
+	II. Supported Web Browsers
+		1. Full Support
+		2. Semi-Support
+	III. Contact and Other Stuff
+		1. Contact
+		2. Other Stuff
+	
+=====================
+=  I. Installation  =
+=====================
+1. Database Setup
+	Currently the provided SQL file assumes an empty database
+	If you have tables in the database you are using that are
+	named the same as any that are used.  They will be dropped
+	and overridden.  You will loose data in that case.  An
+	"Update" sql file is in the works.
+	a. Log into PHPMyAdmin
+	b. Create a new Database, name does not matter
+	c. Click on Import at the top
+	d. Select "Choose File" and navigate to the "setup" directory
+	e. Use "setupNewDatabase.sql"
+	f. Make sure Format is SQL otherwise defaults should be just fine
+
+2. Config Setup
+	a. Open up "config.php" found in the root directory of the
+		authentication server.
+	b. Change the following variables to use your MySQL login data.
+		1. $MySQL['username'] to a MySQL user you have setup for the site.
+		2. $MySQL['password'] to the password for that user.
+		3. $MySQL['database'] to the name of the database you setup.
+		
+	If you are not changing the directory structure of the authentication server
+	you do not need to edit anything else in this file.
+	
+	If you do want to change the directory structure, then read the comments 
+	it should be self-explanatory.
+
+3. Upload Files
+	Use an FTP client such as FileZilla to upload everything 
+		EXCLUDING:
+		1. the setup directory (these files are not needed after initial setup)
+		2. readme.txt (you can upload it if you want but there is no point)
+		3. license.txt (this is for your records there is no need to upload it)
+
+You are DONE! Use your favorite web browser to make sure the site works.  It should
+look similar to http://www.mineshaftersquared.com/MSOpen
+
+================================
+=  II. Supported Web Browsers  =
+================================
+1. Full Support
+	a. Chrome (All Versions)
+	b. Firefox (Version 7 and up)
+	c. Opera (Not Tested, but it should work)
+		- If there are problems contact me. I will fully support this browser, I just have not done extensive testing
+
+2. Semi-Support
+	a. Internet Explorer (Version 9 and Up)
+		- Functionality is supported, but website formatting is not.
+		- Everything should work, it might just look different. (bad).
+
+=================================
+=  III. Contact and Other Stuff =
+=================================
+1. Contact
+	a. Email: Ryan@kayoticlabs.com
+	b. Website: RyanSullivan.org
+	c. MineshafterSquared Website: MineshafterSquared.com
+	d. GitHub: https://github.com/KayoticSully/Open-Mineshafter-Squared
+2. Other Stuff
+	I hope you find Open Mineshafter Squared useful and a pleasure to setup and run.  If you have any problems at all
+	please contact me at the above address, or submit an issue on GitHub.
---
+++ setup/setupNewDatabase.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 18, 2011 at 04:06 PM
+-- Server version: 5.1.59
+-- PHP Version: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `kayoticl_MineshafterSquaredBeta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ActiveCape`
+--
+
+DROP TABLE IF EXISTS `ActiveCape`;
+CREATE TABLE IF NOT EXISTS `ActiveCape` (
+  `userId` int(11) NOT NULL,
+  `capeId` int(11) NOT NULL,
+  UNIQUE KEY `userId` (`userId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ActiveSkin`
+--
+
+DROP TABLE IF EXISTS `ActiveSkin`;
+CREATE TABLE IF NOT EXISTS `ActiveSkin` (
+  `userId` int(11) DEFAULT NULL,
+  `skinId` int(11) DEFAULT NULL,
+  UNIQUE KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Capes`
+--
+
+DROP TABLE IF EXISTS `Capes`;
+CREATE TABLE IF NOT EXISTS `Capes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Data`
+--
+
+DROP TABLE IF EXISTS `Data`;
+CREATE TABLE IF NOT EXISTS `Data` (
+  `property` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `property` (`property`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Data`
+--
+
+INSERT INTO `Data` (`property`, `value`) VALUES
+('client-version', '2.9'),
+('server-version', '2.2'),
+('latest-game-version', '1.8.1'),
+('latest-game-build', '1316075312000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Downloads`
+--
+
+DROP TABLE IF EXISTS `Downloads`;
+CREATE TABLE IF NOT EXISTS `Downloads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `version` float NOT NULL,
+  `trackName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `Downloads`
+--
+
+INSERT INTO `Downloads` (`id`, `name`, `description`, `link`, `type`, `version`, `trackName`) VALUES
+(7, 'Windows Launcher', 'The Windows proxy client.', 'downloads/MineshafterSquared-Client.exe', 'client', 2.9, ''),
+(2, 'Server Proxy', 'The program that makes a Minecraft Server to use this Authentication Server ', 'downloads/MineshafterSquared-Server.jar', 'main', 2.2, ''),
+(3, 'Authentication Server', 'PHP and MySQL based Minecraft Authentication Server', 'downloads/MineshafterSquared-Authentication-Server.zip', 'main', 1.3, ''),
+(4, 'Source Code', 'Because this is open source', 'downloads/Source.zip', 'main', 1.2, ''),
+(5, 'Mac OSX Launcher', 'The Mac OS X proxy client', 'downloads/MineshafterSquared-Client.dmg', 'client', 2.9, ''),
+(6, 'Client Proxy Jar', 'The Mineshafter Squared Java app. (Not needed if you are using a launcher)', 'downloads/MineshafterSquared-Client.jar', 'client', 2.9, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Skins`
+--
+
+DROP TABLE IF EXISTS `Skins`;
+CREATE TABLE IF NOT EXISTS `Skins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `Skins`
+--
+
+INSERT INTO `Skins` (`id`, `userId`, `name`, `description`, `link`) VALUES
+(1, 0, 'Default', 'Default Minecraft Skin', 'Default.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE IF NOT EXISTS `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `session` varchar(255) DEFAULT NULL,
+  `server` varchar(255) DEFAULT NULL,
+  `createDate` int(11) NOT NULL,
+  `lastGameLogin` int(11) NOT NULL,
+  `lastWebLogin` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
